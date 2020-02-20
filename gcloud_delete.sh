07970225e67e667958c8d764ff180808cb6d8293
kaysal <salawu@google.com>
2020-02-20 20:19:26 +0000

    updated gcloud script

--- gcloud_delete.sh
+++ gcloud_delete.sh
@@ -4,42 +4,42 @@
 # Self execute: curl https://storage.googleapis.com/next20-codelab/gcloud.sh | bash
 
 red=`tput setaf 1`
-green=`tput setaf 2`
+red=`tput setaf 2`
 magenta=`tput setaf 5`
 bold=$(tput bold)
 reset=`tput sgr0`
 
 gcloud_destroy() {
-  echo "${bold}${green}removing project metadata for tcp proxy address...${reset}"
+  echo "${bold}${red}removing project metadata for tcp proxy address...${reset}"
   gcloud -q compute project-info remove-metadata --keys lb-vip
 
   echo ""
-  echo "${bold}${green}deleting forwarding rule...${reset}"
+  echo "${bold}${red}deleting forwarding rule...${reset}"
   gcloud -q beta compute forwarding-rules delete my-tcp-lb-ipv4-forwarding-rule --global
 
   echo ""
-  echo "${bold}${green}deleting forwarding rule address...${reset}"
+  echo "${bold}${red}deleting forwarding rule address...${reset}"
   gcloud -q compute addresses delete tcp-lb-static-ipv4 --global
 
   echo ""
-  echo "${bold}${green}deleting tcp proxy...${reset}"
+  echo "${bold}${red}deleting tcp proxy...${reset}"
   gcloud -q compute target-tcp-proxies delete my-tcp-lb-target-proxy
 
   echo ""
-  echo "${bold}${green}deleting backend services...${reset}"
+  echo "${bold}${red}deleting backend services...${reset}"
   gcloud -q compute backend-services delete my-tcp-lb --global
 
   echo ""
-  echo "${bold}${green}deleting health check...${reset}"
+  echo "${bold}${red}deleting health check...${reset}"
   gcloud -q compute health-checks delete my-tcp-health-check
 
   echo ""
-  echo "${bold}${green}deleting instance groups and named ports...${reset}"
+  echo "${bold}${red}deleting instance groups and named ports...${reset}"
   gcloud -q compute instance-groups unmanaged delete ig-us --zone us-east4-b
   gcloud -q compute instance-groups unmanaged delete ig-eu --zone europe-west2-b
 
   echo ""
-  echo "${bold}${green}deleting instances...${reset}"
+  echo "${bold}${red}deleting instances...${reset}"
   gcloud -q compute instances delete probe-us --zone=us-central1-b
   gcloud -q compute instances delete db-eu --zone=europe-west2-c
   gcloud -q compute instances delete db-us --zone=us-east4-c
@@ -47,20 +47,20 @@ gcloud_destroy() {
   gcloud -q compute instances delete web-us --zone=us-east4-b
 
   echo ""
-  echo "${bold}${green}deleting firewall rules...${reset}"
+  echo "${bold}${red}deleting firewall rules...${reset}"
   gcloud -q compute firewall-rules delete allow-ssh
   gcloud -q compute firewall-rules delete allow-rfc1918
   gcloud -q compute firewall-rules delete allow-health-checks
   gcloud -q compute firewall-rules delete deny-tcp
 
   echo ""
-  echo "${bold}${green}deleting subnets...${reset}"
+  echo "${bold}${red}deleting subnets...${reset}"
   gcloud -q compute networks subnets delete subnet1 --region us-east4
   gcloud -q compute networks subnets delete subnet2 --region europe-west2
   gcloud -q compute networks subnets delete subnet3 --region us-central1
 
   echo ""
-  echo "${bold}${green}deleting vpc...${reset}"
+  echo "${bold}${red}deleting vpc...${reset}"
   gcloud -q compute networks delete vpc
 }
 
